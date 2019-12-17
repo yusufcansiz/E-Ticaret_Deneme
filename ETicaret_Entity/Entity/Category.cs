4af59026672e65354e82c8a491199efe5733bbd2
Yusuf Cansız <yusuf.cansiz@outlook.com.tr>
2019-12-17 20:21:23 +0300

    add entity

---
+++ ETicaret_Entity/Entity/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaret_Entity.Entity
+{
+    public class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public int MainCategoryId { get; set; }
+
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ ETicaret_Entity/Entity/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace ETicaret_Entity.Entity
+{
+    public class Product
+    {
+        public Product()
+        {
+            Comments = new HashSet<Comment>();
+
+            Images = new HashSet<Image>();
+        }
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public string ProductProperty { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+
+        public virtual ICollection<Image> Images { get; set; }
+    }
+}
+
+///aciklama
